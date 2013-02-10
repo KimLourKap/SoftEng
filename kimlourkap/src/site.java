7d2573d1dc797b7c4323e1eb877103b81f309904
nikos <nikos.pces@gmail.com>
2013-02-10 21:44:19 +0200

    GUI changes

--- kimlourkap/src/site.java
+++ kimlourkap/src/site.java
@@ -1,12 +1,10 @@
-
-
 import java.awt.BorderLayout;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.JPanel;import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
@@ -16,23 +14,25 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
-
-
-
 public class site extends JFrame {
 
 	private JPanel contentPane;
 
 	public site() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 604, 409);
+		setBounds(100, 100, 604, 430);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JButton btnLogIn = new JButton("log in");
+		btnLogIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("test");
+			}
+		});
+		
 		btnLogIn.setBounds(393, 11, 89, 23);
 		contentPane.add(btnLogIn);
 
@@ -40,46 +40,30 @@ public class site extends JFrame {
 		btnLogOut.setBounds(492, 11, 89, 23);
 		contentPane.add(btnLogOut);
 
-		JButton btnSignUp = new JButton("sign up");
-		btnSignUp.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent arg0) {
-			    JTextField username = new JTextField(5);
-			       JTextField pass = new JTextField(5);
-			    
-			       JPanel myPanel = new JPanel();
-			         myPanel.add(new JLabel("Username:"));
-			         myPanel.add(username);
-			         myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-			         myPanel.add(new JLabel("Password:"));
-			         myPanel.add(pass);
-			         
-			        int result = JOptionPane.showConfirmDialog(null, myPanel, 
-			                  "Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
-			        
-			        String UserName = username.getText();
-			        String Password = pass.getText();
-			        
-			        if (result == JOptionPane.OK_OPTION) {
-			            System.out.println("Username " + UserName);
-			            System.out.println("Password: " + Password);
-			         }
-			   }
-			  });
-			
-		
-		btnSignUp.setBounds(10, 11, 89, 23);
-		contentPane.add(btnSignUp);
-
 		JButton btnReview1 = new JButton("Make Review");
+		btnReview1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JTextArea textAr = new JTextArea("",20,50);
+
+				int okbtn = JOptionPane.showConfirmDialog(null,
+						textAr,
+						"Δημιουργία Review",
+						JOptionPane.OK_CANCEL_OPTION);
+
+				if (okbtn == JOptionPane.OK_OPTION) {
+					String text =  textAr.getText();
+					System.out.println(text);
+
+				}
+
+			}
+		});
+		
 		btnReview1.setBounds(142, 202, 120, 23);
 		contentPane.add(btnReview1);
 
-
-
-
-
-
+		
 		final ImageIcon icon1 = 
 				new ImageIcon(getClass().getResource("\\resources\\icon1.png"));
 		final ImageIcon icon2 = 
@@ -101,8 +85,9 @@ public class site extends JFrame {
 		textArea1.setLineWrap(true);
 		textArea1.setEditable(false);
 		textArea1.setRows(20);
+		textArea1.setText("");
 		scrollPane1.setViewportView(textArea1);
-		
+
 		JButton btnReview2 = new JButton("Make Review");
 		btnReview2.setBounds(461, 202, 120, 23);
 		contentPane.add(btnReview2);
@@ -124,7 +109,7 @@ public class site extends JFrame {
 		textArea2.setRows(20);
 		scrollPane2.setViewportView(textArea2);
 		textArea2.setEditable(false);
-		textArea2.setLineWrap(true);		
+		textArea2.setLineWrap(true);
 		textArea2.setText("");
 
 		JLabel labelDescrip1 = new JLabel("\u039D\u03AD\u03B1 \u03C3\u03B5\u03B9\u03C1\u03AC " +
@@ -138,9 +123,90 @@ public class site extends JFrame {
 		labelDescrip2.setHorizontalAlignment(SwingConstants.CENTER);
 		labelDescrip2.setBounds(318, 183, 270, 14);
 		contentPane.add(labelDescrip2);
+
 		
 		
+		
+		JButton btnNewButton = new JButton("Sign up");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JTextField username = new JTextField(10);
+				JPasswordField pass = new JPasswordField(10);  
+
+				JPanel myPanel = new JPanel();
+				myPanel.add(new JLabel("Username:"));
+				myPanel.add(username);
+				myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+				myPanel.add(new JLabel("Password:"));
+				myPanel.add(pass);
+
+				int result = JOptionPane.showConfirmDialog(null, myPanel, 
+						"Εισάγετε όνομα χρήστη και κωδικό", JOptionPane.OK_CANCEL_OPTION);
+
+				String UserName = username.getText();
+				String Password =  new String(pass.getPassword());
+
+				
+				if (result == JOptionPane.OK_OPTION) {
+
+					if ( UserName == "" || Password == "" ){
+
+						JOptionPane.showMessageDialog(null,"error");
+					}
+					else{
+
+						JOptionPane.showMessageDialog(null,"Γεια σου "+UserName+" ,η εγγραφή ολοκληρώθηκε!");
+
+						System.out.println("Username " + UserName);
+						System.out.println("Password: " + Password);
+					}
+				}
+			}
+		});
+		
+		btnNewButton.setBounds(10, 11, 89, 23);
+		contentPane.add(btnNewButton);
+
+		JButton btnNewButton_1 = new JButton("Rate");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] choices = { "-","1", "2", "3", "4", "5", "6","7","8","9","10" };
+
+				String input = (String) JOptionPane.showInputDialog(null, "Διάλεξε Βαθμολογία",
+						"Βαθμολογία", JOptionPane.QUESTION_MESSAGE, null,
+
+						choices, // Array of choices
+						choices[0]); // Initial choice
+
+				while (input == "-"){
+					JOptionPane.showMessageDialog(null,"Δεν έχετε σημειώσει βαθμολογία");
+					input = (String) JOptionPane.showInputDialog(null, "Choose now...",
+							"Βαθμολογία", JOptionPane.QUESTION_MESSAGE, null,
+							choices, // Array of choices
+							choices[0]); // Initial choice
+				}
+				JOptionPane.showMessageDialog(null,"Η βαθμολογία σας καταχωρήθηκε ");
+				System.out.println(input);  
+			}
+		});
+		
+		
+		btnNewButton_1.setBounds(173, 368, 89, 23);
+		contentPane.add(btnNewButton_1);
+
+		JButton btnRate = new JButton("Rate");
+		btnRate.setBounds(492, 368, 89, 23);
+		contentPane.add(btnRate);
 	}
 
-
+	//private class SwingAction extends AbstractAction {
+	//	public SwingAction() {
+		//	putValue(NAME, "SwingAction");
+		//	putValue(SHORT_DESCRIPTION, "Some short description");
+		//}
+	//	public void actionPerformed(ActionEvent e) {
+	//	}
+	//}
 }
+
